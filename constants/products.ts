42f7658d8e5ad0af612f4aeec0713669a91c3c8a
MiguelP04 <miguel09112004@gmail.com>
2026-02-02 19:04:40 -0400

    SCRUM-2: generate the folder structure

---
+++ constants/products.ts
@@ -0,0 +1,11 @@
+export const PRODUCTS = [
+    {
+        id: "1",
+        name: "Reloj Elegance Gold",
+        price: 1250,
+        image: "/images/reloj.jpg",
+        description: "Un toque de distinción para tu muñeca.",
+        category: "Relojes"
+    },
+    // Agrega más productos aquí...
+];
